f0b355f091c9785bba93c1ad0d3acf96141787e5
Davi Koscianski Vidal <davividal@gmail.com>
2016-04-12 19:47:27 -0300

    Adicionando activities

---
+++ app/src/main/java/br/com/davividal/msport/DetalhesProduto.java
@@ -0,0 +1,13 @@
+package br.com.davividal.msport;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetalhesProduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_produto);
+    }
+}
---
+++ app/src/main/java/br/com/davividal/msport/Relatorio.java
@@ -0,0 +1,13 @@
+package br.com.davividal.msport;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Relatorio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorio);
+    }
+}
